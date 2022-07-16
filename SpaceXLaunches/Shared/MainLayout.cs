5dcb2d8bec9131b59836edf6226aedcee7df3bd9
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-07-16 18:55:26 +0300

    style: add mudblazor

---
+++ SpaceXLaunches/Shared/MainLayout.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceXLaunches.Shared
+{
+    public partial class MainLayout
+    {
+        bool _drawerOpen = true;
+
+
+        void DrawerToggle()
+        {
+            _drawerOpen = !_drawerOpen;
+        }
+    }
+}
